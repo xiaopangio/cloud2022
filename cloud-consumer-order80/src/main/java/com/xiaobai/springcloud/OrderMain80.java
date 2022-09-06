6aeceba155d6b17349377ea210fd1bff7c02bf0c
wanggang <2797491497@qq.com>
2022-09-06 10:23:08 +0800

    eureka3.0以上,默认不再集成ribbon，而是集成了loadblance，再引入ribbon会产生依赖冲突，springcloud2021的eureka的版本在3.0以上。解决方法只能降低springcloud的版本。

--- cloud-consumer-order80/src/main/java/com/xiaobai/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/xiaobai/springcloud/OrderMain80.java
@@ -1,9 +1,11 @@
 package com.xiaobai.springcloud;
 
+import com.xiaobai.springcloud.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * className: OrderMain80
@@ -14,7 +16,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
+@RibbonClient(value = "CLOUD-PAYMENT-SERVICE",configuration = RibbonConfig.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
